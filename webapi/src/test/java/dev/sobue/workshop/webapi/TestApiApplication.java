4e834ec1203d64207e4bb6eb81fa5e99d90830a5
SOBUE Sho <sho@sobue.dev>
2024-11-29 08:36:16 +0900

    Initial commit

---
+++ webapi/src/test/java/dev/sobue/workshop/webapi/TestApiApplication.java
@@ -0,0 +1,14 @@
+package dev.sobue.workshop.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication
+        .from(ApiApplication::main)
+        .run(args);
+  }
+}
