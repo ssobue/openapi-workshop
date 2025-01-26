fde719555a05ce004a23238e22437c98f8a05306
SOBUE Sho <sho@sobue.dev>
2025-01-26 18:41:34 +0900

    update codes

--- webapi/src/main/java/dev/sobue/workshop/webapi/controller/impl/OrdersApiController.java
+++ webapi/src/main/java/dev/sobue/workshop/webapi/controller/impl/OrdersApiController.java
@@ -1,6 +1,10 @@
 package dev.sobue.workshop.webapi.controller.impl;
 
 import dev.sobue.workshop.webapi.controller.OrdersApi;
+import dev.sobue.workshop.webapi.model.Order;
+import dev.sobue.workshop.webapi.model.OrderItem;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,4 +15,67 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrdersApiController implements OrdersApi {
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<List<Order>> getAllOrders() {
+    return OrdersApi.super.getAllOrders();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Order> getOrder(Long orderId) {
+    return OrdersApi.super.getOrder(orderId);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<OrderItem> addOrderItem(Long orderId, OrderItem orderItem) {
+    return OrdersApi.super.addOrderItem(orderId, orderItem);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Order> registerOrder(Order order) {
+    return OrdersApi.super.registerOrder(order);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Void> deleteOrder(Long orderId) {
+    return OrdersApi.super.deleteOrder(orderId);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Order> getOrderTotal(Long orderId, String currency) {
+    return OrdersApi.super.getOrderTotal(orderId, currency);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<OrderItem> updateOrderItem(Long orderId, Long itemId, OrderItem orderItem) {
+    return OrdersApi.super.updateOrderItem(orderId, itemId, orderItem);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Void> deleteOrderItem(Long orderId, Long itemId) {
+    return OrdersApi.super.deleteOrderItem(orderId, itemId);
+  }
 }
--- webapi/src/main/java/dev/sobue/workshop/webapi/controller/impl/ProductsApiController.java
+++ webapi/src/main/java/dev/sobue/workshop/webapi/controller/impl/ProductsApiController.java
@@ -1,6 +1,9 @@
 package dev.sobue.workshop.webapi.controller.impl;
 
 import dev.sobue.workshop.webapi.controller.ProductsApi;
+import dev.sobue.workshop.webapi.model.Product;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,4 +14,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductsApiController implements ProductsApi {
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<List<Product>> getAllProducts() {
+    return ProductsApi.super.getAllProducts();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Product> getProduct(Long productId) {
+    return ProductsApi.super.getProduct(productId);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Product> registerProduct(Product product) {
+    return ProductsApi.super.registerProduct(product);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Product> updateProducts(Long productId) {
+    return ProductsApi.super.updateProducts(productId);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResponseEntity<Void> deleteProduct(Long productId) {
+    return ProductsApi.super.deleteProduct(productId);
+  }
 }
